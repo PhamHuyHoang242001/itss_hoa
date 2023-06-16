706324e80aae9ffb3ea056a821d5f7f88391ff4e
huyhoang242001 <huyhoang.pham@secomus.com>
2023-06-17 01:01:32 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginShow from './component/LoginShow';
+import RegisterShow from './component/RegisterShow';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          
+          <Route path="/login" element={<LoginShow />} />
+          <Route path="/signup" element={<RegisterShow />} />
+          <Route path="/" element={<h1>hello</h1>}></Route>
+
+        </Routes>
+      </Router>
+    </>
   );
 }
-
 export default App;
